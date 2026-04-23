bc3f44f54c91a1f8c7a214a73725cca0e785efc5
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-04-23 15:08:07 +0500

    move api url to environment variable

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL!, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
