c494a7453d7022c8a559fe99caa39fd1d5a62e01
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-04-24 15:52:12 +0500

    fixes

--- services/form.service.ts
+++ services/form.service.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import type { FormFields } from '@/store/store.interface';
 
 export const submitContactForm = async (values: FormFields) => {
-    return await axios.post(process.env.NEXT_PUBLIC_API_URL!, values).then((res) => res.data);
+  return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/posts`, values).then((res) => res.data);
 };
