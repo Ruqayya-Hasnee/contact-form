f6afab1a5378b48dfa210283103eb1c5bae86d55
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-04-24 11:59:56 +0500

    setup ant design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Toaster } from "sonner";
 import "./globals.css";
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
-        <Toaster position="top-center" richColors />
+        <AntdRegistry>
+          {children}
+          <Toaster position="top-center" richColors />
+        </AntdRegistry>
       </body>
     </html>
   );
